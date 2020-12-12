130fa085678f16a8f3463e21ad782f07d81bdad6
Pete Guyatt <pete.guyatt@fcc-fac.ca>
2020-12-12 14:36:16 -0800

    Added comments to explain the task

--- src/main/java/ca/capstone/http/HttpController.java
+++ src/main/java/ca/capstone/http/HttpController.java
@@ -1,8 +1,36 @@
 package ca.capstone.http;
 
+import java.io.InputStream;
+
 public class HttpController {
 
+	/**
+	 * Method to process the http request and return the necessary http response
+	 * @param httpRequest The http request
+	 * @return The response for the request
+	 */
 	public HttpResponse requestResponse(HttpRequest httpRequest) {
+		// For a given http request return a valid http response
+		// Hint: you can switch on the HttpMethod since it's an enum E.g. switch (httpRequest.getMethod()) or you can
+		// use it/else if/else if you so choose.
+		// I would recommend breaking this into methods E.g. processGet/processPost etc
+		// You could embed a full html web page and it's images if you want - look at
+		// HttpController.class.getClassLoader().getResourceAsStream() or you can use the java.io.File to
+		// point to a location on your filesystem
+		// feel free to use something like this:
+		/**
+		return new HttpResponse() {
+			@Override
+			public String contentType() {
+				return null; // This is f
+			}
+
+			@Override
+			public InputStream file() {
+				return null;
+			}
+		};
+		 */
 		throw new UnsupportedOperationException("TO BE IMPLEMENTED");
 	}
 	
--- src/main/java/ca/capstone/http/HttpRequestParser.java
+++ src/main/java/ca/capstone/http/HttpRequestParser.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 public class HttpRequestParser {
 
 	public static HttpRequest parse(BufferedReader reader) throws IOException {
+		// look at reader.lines() which returns a Stream<String> or use a loop and use readLine()
+		// the reader will be the request from the browser and will follow the HTTP spec as detailed
+		// in https://tools.ietf.org/html/rfc2616#section-4
+		// once you have that return a HttpRequest using an nested class (inner, anonymous etc) or a concrete outer
+		// class see - https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html
 		throw new UnsupportedOperationException("TO BE IMPLEMENTED");
 	}
 	
